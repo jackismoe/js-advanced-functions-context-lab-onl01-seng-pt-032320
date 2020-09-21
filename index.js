8a467cbde2cc45b799f44127efffb0a26c0ef25f
jackismoe <jacksonmo@me.com>
2020-09-21 11:46:36 -0400

    Lab Submit

--- index.js
+++ index.js
@@ -1,5 +1,64 @@
 /* Your Code Here */
+let createEmployeeRecord = function(row) {
+  return {
+    firstName: row[0],
+    familyName: row[1],
+    title: row[2],
+    payPerHour: row[3],
+    timeInEvents: [],
+    timeOutEvents: []
+  }
+}
 
+let createEmployeeRecords = function(employeeRowData) {
+  return employeeRowData.map(function(row) {
+    return createEmployeeRecord(row)
+  })
+}
+
+let createTimeInEvent = function(dateStamp) {
+  let [date, hour] = dateStamp.split(' ')
+
+  this.timeInEvents.push({
+    type: "TimeIn",
+    hour: parseInt(hour, 10),
+    date,
+  })
+
+  return this
+}
+let createTimeOutEvent = function(dateStamp) {
+  let [date, hour] = dateStamp.split(' ')
+
+  this.timeOutEvents.push({
+    type: "TimeOut",
+    hour: parseInt(hour, 10),
+    date,
+  })
+
+  return this
+}
+
+let hoursWorkedOnDate = function(soughtDate) {
+  let inEvent = this.timeInEvents.find(e => e.date === soughtDate)
+  let outEvent = this.timeOutEvents.find(e => e.date === soughtDate)
+  return (outEvent.hour - inEvent.hour)/100
+}
+
+let wagesEarnedOnDate = function(soughtDate) {
+  let rawWage = hoursWorkedOnDate.call(this, soughtDate) * this.payPerHour
+  return parseFloat(rawWage.toString())
+}
+
+let findEmployeeByFirstName = function(srcArray, firstName) {
+  return srcArray.find(rec =>  rec.firstName === firstName)
+}
+
+let calculatePayroll = function(arrayOfEmployeeRecords) {
+  return arrayOfEmployeeRecords.reduce(function(memo, rec){
+    return memo + allWagesFor.call(rec)
+  }, 0)
+}
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
